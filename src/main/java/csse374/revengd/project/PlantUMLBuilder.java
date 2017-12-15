34239557fa3a7f8b09955f792291340a402f94c9
Jacob Gathof <gathofjd@rose-hulman.edu>
2017-12-15 15:53:19 -0500

    Minor changes

--- src/main/java/csse374/revengd/project/PlantUMLBuilder.java
+++ src/main/java/csse374/revengd/project/PlantUMLBuilder.java
@@ -19,6 +19,8 @@ public class PlantUMLBuilder implements IBuilder {
         	builder.append(s + "\n");
         }
         
+        System.out.println(builder.toString());
+        
         builder.append("@enduml");
         return builder.toString();
     }
