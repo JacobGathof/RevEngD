2aed89389577eb1bf524d2609de66b5894eabda0
Trevor Morton <mortontc@rose-hulman.edu>
2018-01-24 13:47:26 -0500

    Damn it jack

---
+++ src/main/java/csse374/revengd/project/App.java
@@ -0,0 +1,23 @@
+package csse374.revengd.project;
+
+import csse374.revengd.project.builder.IBuilder;
+import csse374.revengd.project.displayer.IDisplayer;
+import csse374.revengd.project.parsers.IParser;
+import csse374.revengd.project.parsers.MasterParser;
+import csse374.revengd.project.parserstrategies.IParserStrategy;
+
+import java.util.List;
+
+public class App {
+    public static void main(String[] args){
+        Configuration config = new Configuration(args);
+        List<IParserStrategy> strategies = config.getStrategies();
+        IBuilder builder = config.getBuilders();
+        IDisplayer displayer = config.getDisplayers();
+        IParser parser = new MasterParser(config.getPath(), strategies);
+        parser = config.applyFilters(parser);
+        parser = config.applyDetectors(parser);
+        Runner runner = new Runner(parser, builder, displayer);
+        runner.run(config.getClasses());
+    }
+}
